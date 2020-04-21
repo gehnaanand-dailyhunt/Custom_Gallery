63133f1335916520ad0d7ed7262468e80d84eeba
gehnaanand-dailyhunt <gehna.anand@dailyhunt.in>
2020-04-21 13:15:47 +0530

    Flickr API

--- app/src/main/java/com/example/mediagallery/flickr/adapter/PhotoAdapter.kt
+++ app/src/main/java/com/example/mediagallery/flickr/adapter/PhotoAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
--- app/src/main/java/com/example/mediagallery/flickr/model/Photo.kt
+++ app/src/main/java/com/example/mediagallery/flickr/model/Photo.kt
@@ -1,10 +1,17 @@
 package com.example.mediagallery.flickr.model
 
 import androidx.recyclerview.widget.DiffUtil
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
 
 data class Photo constructor(
+    @field:SerializedName("id")
     val id: String,
+    @field:SerializedName("url")
     val url: String,
+    @field:SerializedName("title")
     val title: String) {
 
     companion object {
